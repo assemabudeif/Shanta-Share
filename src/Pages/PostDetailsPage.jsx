3d4d0e16d53f64a46e47c24b1b4594da84496c00
Assem Ashraf <assemabudeif@gmail.com>
2024-08-16 19:57:19 +0300

    install tailwind

--- src/Pages/PostDetailsPage.jsx
+++ src/Pages/PostDetailsPage.jsx
@@ -1,14 +1,13 @@
 import {useParams} from "react-router-dom";
 
 function PostDetailsPage() {
-  const { id } = useParams();
-
+    const {id} = useParams();
     return (
         <div>
             <h1>Post Details Page</h1>
             <p>Post ID: {id}</p>
         </div>
-        );
+    );
 }
 
 export default PostDetailsPage;
