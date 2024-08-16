382977ebe70f42c14ac44840f2219ee504c7af2b
Assem Ashraf <assemabudeif@gmail.com>
2024-08-16 20:03:53 +0300

    chore: add not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import PostDetailsPage from "./Pages/PostDetailsPage";
+import PageNotFound from "./Pages/PageNotFound";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path={"/post/:id"} element={<PostDetailsPage />} />
+
+            <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
