ecd953c4268b753a4f44a4e11d76d7306fb6e341
Esraa Adel <ekoryem@gmail.com>
2024-08-19 13:02:23 +0300

    profile-driver-copy1

--- src/components/Reviews/addReview.js
+++ src/components/Reviews/addReview.js
@@ -27,7 +27,7 @@ const AddReview = ({ onSubmit }, review) => {
     <>
       <button
         onClick={() => setIsOpen(true)}
-        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200"
+        className=" ml-70 mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200"
       >
         Add Review
       </button>
--- src/components/Reviews/reviewList.js
+++ src/components/Reviews/reviewList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import DriverData from '../../data/driverData.json';
 import ReviewCard from './reviewCard';
 import AddReview from './addReview';
+import '../../CSS/reviewCard.css'
 
 const ReviewsList = () => {
   const [reviews, setReviews] = useState([]);
@@ -20,19 +21,21 @@ const ReviewsList = () => {
   };
 
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col">
       {reviews.slice(0, showMore ? reviews.length : 1).map((review, index) => (
         <ReviewCard key={index} review={review} />
       ))}
-      {reviews.length > 1 && (
-        <button
-          onClick={handleShowMore}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200 btnShow"
-        >
-          {showMore ? 'Show Less' : 'Show More'}
-        </button>
-      )}
-      <AddReview onSubmit={handleAddReview} />
+      <div className="mt-2 ml-20">
+        {reviews.length > 1 && (
+          <button
+            onClick={handleShowMore}
+            className=" ml-70 mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200 btnShow"
+          >
+            {showMore ? 'Show Less' : 'Show More'}
+          </button>
+        )}
+        <AddReview onSubmit={handleAddReview} />
+      </div>
     </div>
   );
 };
