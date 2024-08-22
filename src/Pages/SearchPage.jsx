65c39d2c628fb8fc8274557504f3fcafed9b8054
Assem Ashraf <assemabudeif@gmail.com>
2024-08-23 02:47:52 +0300

    chore: fetch search posts data from api

--- src/Pages/SearchPage.jsx
+++ src/Pages/SearchPage.jsx
@@ -224,7 +224,7 @@ export default function SearchPage() {
                                             <span className={"text-4xl font-semibold"}>
                                                 {post.price}.00 L.E
                                             </span>
-                                                <Link to={"/post/1"} className={"mx-8"}>
+                                                <Link to={"/post/1"} state={post} className={"mx-8"}>
                                                     <svg width="16" height="28" viewBox="0 0 16 28" fill="none"
                                                          xmlns="http://www.w3.org/2000/svg">
                                                         <path
