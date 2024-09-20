b2be97ca7f0ab977275c9eb6f201de1d4218eada
Assem Ashraf <assemabudeif@gmail.com>
2024-09-20 23:28:48 +0300

    chore: add toke to AxiosInstance

--- src/Network/AxiosInstance.jsx
+++ src/Network/AxiosInstance.jsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 import Store from "../Store/Store";
-import {setLoader} from "../Store/Actions/LoaderAction ";
+import { setLoader } from "../Store/Actions/LoaderAction ";
+
+const token = localStorage.getItem("token") ? `Bearer ${localStorage.getItem("token")}` : "";
+
 
 export const AxiosInstance = axios.create({
     baseURL: "http://127.0.0.1:8000",
+    headers: {
+        Authorization: token,
+    }
 });
-
 
 AxiosInstance.interceptors.request.use(function (config) {
 
