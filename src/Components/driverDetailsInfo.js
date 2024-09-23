458d8521bd44f7099f015f88200fa34a354ed925
Esraa Adel <ekoryem@gmail.com>
2024-09-23 15:25:02 +0300

    add translation to driver home page

--- src/Components/driverDetailsInfo.js
+++ src/Components/driverDetailsInfo.js
@@ -1,27 +1,29 @@
 import React from "react";
 import '../CSS/driverProfile.css';
+import { useTranslation } from 'react-i18next';
 
 function DriverDetailsInfo(props) {
+  const { t, i18n } = useTranslation();
   return (
     <div className="grid grid-cols-3 gap-20 d-md-grid">
       <div className="detailsInfo mr-20">
-        <span className="text-gray font-semibold text-l ml-20 titleInfo">From: <span className="text-gray valueInfo">{props.travelPost.address}</span></span><br />
-        <span className="text-gray font-semibold text-l ml-20 titleInfo">Licence Type: <span className="valueInfo">{props.travelPost.licenseType}</span></span>
+        <span className="text-gray font-semibold text-l ml-20 titleInfo">{t("driverProfile.From")} <span className="text-gray valueInfo">{props.travelPost.address}</span></span><br />
+        <span className="text-gray font-semibold text-l ml-20 titleInfo">{t("driverProfile.LicenceType")} <span className="valueInfo">{props.travelPost.licenseType}</span></span>
       </div>
       <div className="col-span-2 flex justify-center">
         <div className="text-end">
           <div className="flex space-x-20 gap-20">
             <div className="text-end">
               <span className="block text-2xl font-bold fontSize">{props.travelPost.totalTrips}</span>
-              <span className="text-gray-500 ml-2">Trips</span>
+              <span className="text-gray-500 ml-2">{t("driverProfile.Trips")}</span>
             </div>
             <div>
               <span className="block text-2xl font-bold fontSize">{props.travelPost.driverRating}</span>
-              <span className="text-gray-500 ml-0">Rating</span>
+              <span className="text-gray-500 ml-0">{t("driverProfile.Rating")}</span>
             </div>
             <div>
               <span className="block text-2xl font-bold ml-2 fontSize">{props.travelPost.drivingExperience}</span>
-              <span className="text-gray-500 ml-0">Years</span>
+              <span className="text-gray-500 ml-0">{t("driverProfile.Years")}</span>
             </div>
           </div>
         </div>
